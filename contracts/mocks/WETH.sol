ffd1d90eb6d5a4c2daeffa190486cdd04ce98381
gfijrb <gfijrb@yandex.ua>
2021-07-21 17:12:26 +0300

    rename MockWETH to WETH

--- contracts/mocks/WETH.sol
+++ contracts/mocks/WETH.sol
@@ -2,11 +2,7 @@
 
 pragma solidity ^0.6.0;
 
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract MockWETH {
-    uint256 private constant _INITIAL_SUPPLY = 100000000 * (10**18);
-
+contract WETH {
     string public name = "Wrapped Ether";
     string public symbol = "WETH";
     uint8 public decimals = 18;
